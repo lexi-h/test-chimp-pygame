fa0346e2f93578b369d60e12ad35395377d4037e
Lexi Hattaway <lexhatt1@gmail.com>
2022-03-30 08:47:18 -0500

    added chimp class

--- chimp-punch.py
+++ chimp-punch.py
@@ -71,3 +71,50 @@ class Fist(pygame.sprite.Sprite):
     def unpunch(self):
         """ccalled to pull the fist back"""
         self.punching = False
+        
+class Chimp(pygame.sprite.Sprite):
+    """moves a monkey critter across the screen. it can spin the monkey when it is punched"""
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) #call parent init
+        self.image, self.rect = load_image("chimp.png", -1, 4)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 90
+        self.move = 18
+        self.dizzy = False
+
+    def update(self):
+        """walk or spin, depending on the monkey's state"""
+        if self.dizzy:
+            self._spin()
+        else:
+            self._walk()
+
+    def _walk(self):
+        """move the monkey across the screen, and turn at the ends"""
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+            if self.rect.left < self.area.left or self.rect.right > self.area.right:
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
+                self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = newpos
+
+    def _spin(self):
+        """spint the monkey image"""
+        center = self.rect.center
+        self.dizzy = self.dizzy + 12
+        if self.dizzy >= 360:
+            self.dizzy = False
+            self.image = self.original
+        else:
+            rotate = pygame.transform.rotate
+            self.image = rotate(self.original, self.dizzy)
+        self.rect = self.image.get_rect(center=center)
+
+    def punched(self):
+        """this will cause the monkey to start spinning"""
+        if not self.dizzy:
+            self.dizzy = True 
+            self.original = self.image
